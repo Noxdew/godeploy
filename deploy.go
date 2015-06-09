f53365baa6495a0ee2f3d3361ffc32c1c6d6a669
Georgi <y0c0vc@gmail.com>
2015-06-09 10:54:25 +0100

    Added more options

--- deploy.go
+++ deploy.go
@@ -12,18 +12,20 @@ import (
 )
 
 type Config struct {
-	Endpoint        string
-	Port            string
-	Method          string
-	RepoDir         string
-	RepoBranch      string
-	RepoBranchCheck bool
-	RepoRunScript   string
-	RepoSecret      string
+	ServerEndpoint   string
+	ServerPort       string
+	ServerMethod     string
+	RepoDir          string
+	RepoBranch       string
+	RepoRunScript    string
+	RepoSecret       string
+	RepoBranchCheck  bool
+	ScriptAlwaysWait bool
+	ScriptRunAtStart bool
 }
 
 type GitRes struct {
-	ref string
+	Ref string
 }
 
 func main() {
@@ -43,17 +45,25 @@ func main() {
 	}
 
 	var command = "cd " + conf.RepoDir + " && git checkout " + conf.RepoBranch + " && git pull && " + conf.RepoRunScript
+	var errChan = make(chan error)
 
-	var cmd = exec.Command("sh", "-c", command)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Start()
-	if err != nil {
-		fmt.Println(err)
+	var tempCommand string
+	if conf.ScriptRunAtStart {
+		tempCommand = command
+	} else {
+		tempCommand = ""
 	}
 
-	http.HandleFunc("/"+conf.Endpoint, func(res http.ResponseWriter, req *http.Request) {
-		if req.Method == conf.Method {
+	var cmd = exec.Command("sh", "-c", tempCommand)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	go func() {
+		errChan <- cmd.Run()
+	}()
+
+	http.HandleFunc("/"+conf.ServerEndpoint, func(res http.ResponseWriter, req *http.Request) {
+		if req.Method == conf.ServerMethod {
 			var git GitRes
 
 			if conf.RepoBranchCheck {
@@ -71,20 +81,30 @@ func main() {
 				}
 			}
 
-			if !conf.RepoBranchCheck || git.ref == "refs/heads/"+conf.RepoBranch {
+			if !conf.RepoBranchCheck || git.Ref == "refs/heads/"+conf.RepoBranch {
 				for !cmd.ProcessState.Exited() {
 					err = cmd.Process.Signal(syscall.SIGINT)
 					if err != nil {
 						fmt.Println(err)
 					}
+					select {
+					default:
+						fmt.Println("process still running")
+					case e := <-errChan:
+						if e != nil {
+							fmt.Printf("process exited: %s\n", e)
+						} else {
+							fmt.Println("process exited without error")
+						}
+					}
 				}
 				cmd = exec.Command("sh", "-c", command)
 				cmd.Stdout = os.Stdout
 				cmd.Stderr = os.Stderr
-				err = cmd.Start()
-				if err != nil {
-					fmt.Println(err)
-				}
+
+				go func() {
+					errChan <- cmd.Run()
+				}()
 			}
 			fmt.Fprintf(res, "Done")
 		} else {
@@ -93,11 +113,11 @@ func main() {
 	})
 
 	s := &http.Server{
-		Addr:           ":" + conf.Port,
+		Addr:           ":" + conf.ServerPort,
 		ReadTimeout:    30 * time.Second,
 		WriteTimeout:   30 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	fmt.Println("Listening on port ", conf.Port, " for deployment requests")
+	fmt.Println("Listening on port ", conf.ServerPort, " for deployment requests")
 	fmt.Println(s.ListenAndServe())
 }
